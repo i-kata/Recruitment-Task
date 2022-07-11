b0a3940d1449602ad3e61255b1f94353f9451820
Iwan Kata <iwan.kata@SB-PL-0DQMD6T.local>
2022-07-12 01:19:17 +0200

    Final

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,6 @@
 Vagrant.configure("2") do |config|
+
+  config.vm.network "forwarded_port", guest: 8080, host: 8080
   
   config.vm.box = "ubuntu/focal64"
 
--- create_job.sh
+++ create_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-wget http://localhost:8080/jnlpJars/jenkins-cli.jar
-java -jar jenkins-cli.jar -s http://localhost:8080/ get-job template > template.xml
-java -jar jenkins-cli.jar -s http://localhost:8080/ create-job new_job_name < new_job_name.xml
-java -jar jenkins-cli.jar -s http://localhost:8080/ groovy scripts/add_job_to_view.groovy
-java -jar jenkins-cli.jar -s https://localhost:8080/ -webSocket build ExampleJob
-java -jar jenkins-cli.jar -s http://localhost:8080/ list-jobs tools
+  
+cd ~
+admin_pass="$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)"
+num="$(date +%s)"
+job_name="JOB"$num
+java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:$admin_pass create-job $job_name < job.xml
+echo "$(java -jar jenkins-cli.jar -s http://localhost:8080/ -auth admin:$admin_pass list-jobs)" > ~/current_jobs.txt
