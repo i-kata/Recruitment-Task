7ba66a8163924269d297d5f92bc5edb6bbe86c8c
Iwan Kata <iwan.kata@SB-PL-0DQMD6T.local>
2022-07-11 08:44:21 +0200

    Solution v2

--- create_job.sh
+++ create_job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo wget http://jenkins/jnlpJars/jenkins-cli.jar
-echo java -jar jenkins-cli.jar -s https://localhost:8080/ - webSocket build ExampleJob
+echo wget http://localhost:8080/jnlpJars/jenkins-cli.jar
+echo java -jar jenkins-cli.jar -s https://localhost:8080/ -webSocket build ExampleJob
